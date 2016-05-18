8ea7616ae2bf5180eba0f0f9f1ee76e9a4e9f1ef
Connor Worley <connorbworley@gmail.com>
2016-05-18 11:39:02 -0700

    Revert "Installer"
    This reverts commit 2ef943b3a3f9e2feffbfd2c050833f55f4b7be3a.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mv .zshrc .zshrc.original
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm .zshrc
-mv .zshrc.original .zshrc
-rm install.sh
-killall zsh
-zsh
